6a75ba3ad8b6698ca2af351357896eebaf97fb65
Cleopatro <longforward@qq.com>
2014-10-17 09:50:52 +0800

    test jh

---
+++ jh.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<title>聚合页</title>
+<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="black" />
+<meta content="telephone=no" name="format-detection" />
+<link rel="stylesheet" media="all" href="css/jh.css?141016" />
+</head>
+<body>
+<header>
+	<h1 class="jh_title">为您推荐</h1>
+	<i class="ico-return"></i>
+</header>
+<p class="txt-bar txt-info">左滑可删除您不喜欢的推荐项目</p>
+<div class="main">
+	<div class="noresult" style="display:;">没有找到符合条件的结果</div>
+	<div class="search_panel" style="display:;">
+		<h3>请修改条件重新查询</h3>
+		<div class="search_panel_li clearfix">
+			<!-- 选中状态添加seach_panel-item-on类 -->
+			<span class="search_panel-item seach_panel-item-on">用车</span>
+			<span class="search_panel-item">当地玩乐</span>
+			<span class="search_panel-item">清迈</span>
+			<span class="search_panel-item">东京</span>
+			<span class="search_panel-item">团队游</span>
+		</div>
+	</div>
+	<h3 class="recom-title">推荐给您的酒店</h3>
+	<section class="recom">
+		<div class="recom_prod_wrap">
+			<div class="recom_prod clearfix">
+				<div class="recom_prod-img">
+					<img src="http://pic.c-ctrip.com/distribution/jhpage/prod-img/p1.png">
+				</div>
+				<div class="recom_prod-main">
+					<h2>上海绿地豪生全套房酒店</h2>
+					<div class="recom_prod-info">
+						<p class="hlt-area">陆家嘴金融贸易区</p>
+						<p class="cgray txt-multi">推荐理由：推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐</p>
+					</div>
+					<div class="recom_prod-price cgray">
+						<p class="price-main"><span class="price"><small>&yen;</small>1199</span>起</p>
+						<p class="price-sub"><i class="ico-orange">可返</i><span class="price"><small>&yen;</small>100</span></p>
+						<p><i class="ico-blue">礼</i><i class="ico-blue">券</i></p>
+					</div>
+					<i class="ico-like ico-like-empty"></i>
+				</div>
+				<div class="btn-cancel">删除</div>
+			</div>
+		</div>
+		<div class="recom_prod_wrap">
+			<!-- 左滑出删除按钮添加recom_prod-cancel类 -->
+			<div class="recom_prod clearfix recom_prod-cancel">
+				<div class="recom_prod-img">
+					<img src="http://pic.c-ctrip.com/distribution/jhpage/prod-img/p1.png">
+				</div>
+				<div class="recom_prod-main">
+					<h2>上海绿地豪生全套房酒店</h2>
+					<div class="recom_prod-info">
+						<p class="hlt-area">陆家嘴金融贸易区</p>
+						<p class="cgray txt-multi">推荐理由：推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐</p>
+					</div>
+					<div class="recom_prod-price cgray">
+						<p class="price-main"><span class="price"><small>&yen;</small>1199</span>起</p>
+						<p class="price-sub"><i class="ico-orange">可返</i><span class="price"><small>&yen;</small>100</span></p>
+						<p><i class="ico-blue">礼</i><i class="ico-blue">券</i></p>
+					</div>
+					<i class="ico-like ico-like-full"></i>
+				</div>
+				<div class="btn-cancel">删除</div>
+			</div>
+		</div>
+		<div class="recom_prod_wrap">
+			<div class="recom_prod clearfix">
+				<div class="recom_prod-img">
+					<img src="http://pic.c-ctrip.com/distribution/jhpage/prod-img/p1.png">
+				</div>
+				<div class="recom_prod-main">
+					<h2>上海绿地豪生全套房酒店</h2>
+					<div class="recom_prod-info">
+						<p class="hlt-area">陆家嘴金融贸易区</p>
+						<p class="cgray txt-multi">推荐理由：推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐</p>
+					</div>
+					<div class="recom_prod-price cgray">
+						<p class="price-main"><span class="price"><small>&yen;</small>1199</span>起</p>
+						<p class="price-sub"><i class="ico-orange">可返</i><span class="price"><small>&yen;</small>100</span></p>
+						<p><i class="ico-blue">礼</i><i class="ico-blue">券</i></p>
+					</div>
+					<i class="ico-like ico-like-empty"></i>
+				</div>
+				<div class="btn-cancel">删除</div>
+			</div>
+		</div>
+		<div class="recom_prod_wrap">
+			<div class="recom_prod clearfix">
+				<div class="recom_prod-img">
+					<img src="http://pic.c-ctrip.com/distribution/jhpage/prod-img/p1.png">
+				</div>
+				<div class="recom_prod-main">
+					<h2>上海绿地豪生全套房酒店</h2>
+					<div class="recom_prod-info">
+						<p class="hlt-area">陆家嘴金融贸易区</p>
+						<p class="cgray txt-multi">推荐理由：推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐</p>
+					</div>
+					<div class="recom_prod-price cgray">
+						<p class="price-main"><span class="price"><small>&yen;</small>1199</span>起</p>
+						<p class="price-sub"><i class="ico-orange">可返</i><span class="price"><small>&yen;</small>100</span></p>
+						<p><i class="ico-blue">礼</i><i class="ico-blue">券</i></p>
+					</div>
+					<i class="ico-like ico-like-empty"></i>
+				</div>
+				<div class="btn-cancel">删除</div>
+			</div>
+		</div>
+	</section>
+	<h3 class="recom-title">推荐给您的旅游</h3>
+	<section class="recom">
+		<div class="recom_prod_wrap">
+			<div class="recom_prod clearfix">
+				<div class="recom_prod-img">
+					<img src="http://pic.c-ctrip.com/distribution/jhpage/prod-img/p2.png">
+				</div>
+				<div class="recom_prod-main">
+					<h2 class="txt-multi">浙江千岛湖2日半自助游绿城度假酒店&middot;免一人出行费</h2>
+					<div class="recom_prod-info">
+						<p class="cgray txt-multi">推荐理由：推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐</p>
+					</div>
+					<div class="recom_prod-price cgray">
+						<p class="price-main"><span class="price"><small>&yen;</small>998</span>起</p>
+					</div>
+					<i class="ico-like ico-like-full"></i>
+				</div>
+				<div class="btn-cancel">删除</div>
+			</div>
+		</div>
+		<div class="recom_prod_wrap">
+			<div class="recom_prod clearfix">
+				<div class="recom_prod-img">
+					<img src="http://pic.c-ctrip.com/distribution/jhpage/prod-img/p2.png">
+				</div>
+				<div class="recom_prod-main">
+					<h2 class="txt-multi">浙江千岛湖2日半自助游绿城度假酒店&middot;免一人出行费</h2>
+					<div class="recom_prod-info">
+						<p class="cgray txt-multi">推荐理由：推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐</p>
+					</div>
+					<div class="recom_prod-price cgray">
+						<p class="price-main"><span class="price"><small>&yen;</small>998</span>起</p>
+					</div>
+					<i class="ico-like ico-like-full"></i>
+				</div>
+				<div class="btn-cancel">删除</div>
+			</div>
+		</div>
+	</section>
+	<h3 class="recom-title">推荐给您的机票</h3>
+	<section class="recom">
+		<div class="recom_prod_wrap">
+			<div class="recom_prod recom_prod_noimg">
+				<div class="recom_prod-main">
+					<div class="flt-info clearfix">
+						<div class="flt-start fl">
+			                <p><span>07:00</span></p>
+			                <p>虹桥T2</p>
+			            </div>
+			            <div class="flt-mid fl">
+		                	<i class="flt-ico-fx"></i>
+		                </div>
+		                <div class="flt-end fl">
+			                <p><span>09:10</span></p>
+			                <p>首都T1</p>
+			            </div>
+						<div class="flt-hs clear">
+			                <img src="http://pic.c-ctrip.com/flight_intl/airline_logo/40x35/MU.png" class="ico-flt" />
+			                <span>东航MU9821</span><span>波音773(大)</span>
+			            </div>
+					</div>
+					<div class="recom_prod-info">
+						<p class="cgray txt-multi">推荐理由：推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐</p>
+					</div>
+					<div class="recom_prod-price cgray">
+						<p class="price-main"><span class="price"><small>&yen;</small>2950</span>起</p>
+						<p class="price-sub"><i class="ico-orange-full">返&yen;50</i><i class="ico-blue-full">专享</i></p>
+					</div>
+					<i class="ico-like ico-like-full"></i>
+				</div>
+				<div class="btn-cancel">删除</div>
+			</div>
+		</div>
+		<div class="recom_prod_wrap">
+			<div class="recom_prod recom_prod_noimg">
+				<div class="recom_prod-main">
+					<div class="flt-info clearfix">
+						<div class="flt-start fl">
+			                <p><span>07:00</span></p>
+			                <p>虹桥T2</p>
+			            </div>
+			            <div class="flt-mid fl">
+		                	<i class="flt-ico-fx"></i>
+		                </div>
+		                <div class="flt-end fl">
+			                <p><span>09:10</span></p>
+			                <p>首都T1</p>
+			            </div>
+						<div class="flt-hs clear">
+			                <img src="http://pic.c-ctrip.com/flight_intl/airline_logo/40x35/MU.png" class="ico-flt" />
+			                <span>东航MU9821</span><span>波音773(大)</span>
+			            </div>
+					</div>
+					<div class="recom_prod-info">
+						<p class="cgray txt-multi">推荐理由：推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐理由推荐</p>
+					</div>
+					<div class="recom_prod-price cgray">
+						<p class="price-main"><span class="price"><small>&yen;</small>2950</span>起</p>
+						<p class="price-sub"><i class="ico-orange-full">返&yen;50</i><i class="ico-blue-full">专享</i></p>
+					</div>
+					<i class="ico-like ico-like-full"></i>
+				</div>
+				<div class="btn-cancel">删除</div>
+			</div>
+		</div>
+	</section>
+</div>
+<footer style="">
+	<ul class="filter_bar">
+		<!-- 有筛选条件添加filter_on类，当前状态添加filter_curr类 -->
+		<li class="filter-prod filter_curr">产品类别<i class="ico-filter ico-filter-prod"></i></li>
+		<li class="filter-dest filter_on">目的地<i class="ico-filter ico-filter-dest"></i></li>
+		<li class="filter-price">价格从高到低<i class="ico-filter ico-filter-price"></i></li>
+	</ul>
+</footer>
+<div class="cui-mask" style="display:none;"></div>
+<div class="cui-view cui-layer cui-alert" style="display:none;margin-left: -153px;margin-top: -53px;z-index: 3000;">
+	<div class="cui-pop-box">
+		<div class="cui-bd">
+			<div class="cui-error-tips">将取消对改机票的收藏，同时会从“我收藏的机票”列表中删除。</div>
+			<div class="cui-roller-btns">
+				<div class="cui-flexbd cui-btns-cancel">点错了</div>
+				<div class="cui-flexbd cui-btns-sure">好的</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="cui-view cui-layer cui-alert" style="display:none;margin-left: -153px;margin-top: -53px;z-index: 3000;">
+	<div class="cui-pop-box">
+		<div class="cui-bd">
+			<div class="cui-error-tips">收藏成功</div>
+		</div>
+	</div>
+</div>
+<div class="cui-view cui-layer cui-alert" style="display:none;margin-left: -153px;margin-top: -53px;z-index: 3000;">
+	<div class="cui-pop-box">
+		<div class="cui-bd">
+			<div class="cui-error-tips">删除收藏成功</div>
+		</div>
+	</div>
+</div>
+<div class="filter_box" style="display:none;">
+	<div class="filter_box-title">
+		<div class="filter_box-cancel">取消</div>
+		<div class="filter_box-confirm">确定</div>
+	</div>
+	<ul class="filter_box-list">
+		<!-- 选中状态增加filter_box-item-checked类 -->
+		<li class="filter_box-item filter_box-item-checked">不限<i class="ico-checkbox-bg"></i></li>
+		<li class="filter_box-item filter_box-item-checked">机票<i class="ico-checkbox"></i></li>
+		<li class="filter_box-item">酒店<i class="ico-checkbox"></i></li>
+		<li class="filter_box-item">团队游<i class="ico-checkbox"></i></li>
+		<li class="filter_box-item">自由行<i class="ico-checkbox"></i></li>
+		<li class="filter_box-item">当地玩乐<i class="ico-checkbox"></i></li>
+		<li class="filter_box-item">用车<i class="ico-checkbox"></i></li>
+		<li class="filter_box-item">高端美食<i class="ico-checkbox"></i></li>
+	</ul>
+	<i class="ico-box-triangle"></i>
+</div>
+</body>
+</html>
